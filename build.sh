767f26726d76f4e4816941c287239c585012573d
back-up-git <100161001+back-up-git@users.noreply.github.com>
2022-08-07 04:33:55 +0530

    Try

--- build.sh
+++ build.sh
@@ -1,24 +1,21 @@
 #!bin/bash
 
-
-# Install Required Packages
-sudo apt install make bison bc libncurses5-dev tmate git python3-pip curl build-essential zip unzip -y
-
 # Working Directory
 WORKING_DIR=~/
 
 # Functions For Telegram Post
 msg() {
-	curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
-        -d chat_id="${TG_CHAT_ID}" \
+	curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT_ID" \
 	-d "disable_web_page_preview=true" \
 	-d "parse_mode=html" \
 	-d text="$1"
 }
+
 file() {
 	MD5=$(md5sum "$1" | cut -d' ' -f1)
-	curl --progress-bar -F document=@"$1" "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument" \
-	-d chat_id="${TG_CHAT_ID}" \
+	curl --progress-bar -F document=@"$1" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+	-d chat_id="$TG_CHAT_ID" \
 	-F "disable_web_page_preview=true" \
 	-F "parse_mode=html" \
 	-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5</code>"
@@ -31,7 +28,7 @@ git clone --depth=1 https://github.com/back-up-git/AnyKernel3.git -b main $WORKI
 git clone --depth=1 https://github.com/back-up-git/kernel_xiaomi_raphael.git -b staging $WORKING_DIR/kernel
 
 # Cloning Toolchain
-git clone https://github.com/kdrag0n/proton-clang.git -b main toolchain
+git clone https://github.com/kdrag0n/proton-clang.git toolchain
 
 # Change Directory to the Source Directry 
 cd $WORKING_DIR/kernel
@@ -47,7 +44,7 @@ COMMIT_LOG=$(git log --oneline -n 1)
 COMPILER=$($WORKING_DIR/toolchains/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 #Starting Compilation
-msg "<b>========IMMENSiTY-ext-RAPHAEL========</b>%0A<b>Hey Immanuel!! Kernel Build Triggered !!</b>%0A<b>Device: </b><code>$DEVICE</code>%0A<b>Kernel Version: </b><code>$VERSION</code>%0A<b>Date: </b><code>$DATE</code>%0A<b>Host Distro: </b><code>$DISTRO</code>%0A<b>Host Core Count: </b><code>$CORES</code>%0A<b>Compiler Used: </b><code>$COMPILER</code>%0A<b>Branch: </b><code>$BRANCH</code>%0A<b>Last Commit: </b><code>$COMMIT_LOG</code>"
+msg "<b>========IMMENSiTY-ext-RAPHAEL========</b>%0A<b> Kernel Build Triggered !!</b>%0A<b>Device: </b><code>$DEVICE</code>%0A<b>Kernel Version: </b><code>$VERSION</code>%0A<b>Date: </b><code>$DATE</code>%0A<b>Host Distro: </b><code>$DISTRO</code>%0A<b>Host Core Count: </b><code>$CORES</code>%0A<b>Compiler Used: </b><code>$COMPILER</code>%0A<b>Branch: </b><code>$BRANCH</code>%0A<b>Last Commit: </b><code>$COMMIT_LOG</code>"
 BUILD_START=$(date +"%s")
 export ARCH=arm64
 export KBUILD_BUILD_USER="Azure"
