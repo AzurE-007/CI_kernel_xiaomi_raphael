bfabec58f3e95974fb4969177517b8c29cb4c707
back-up-git <100161001+back-up-git@users.noreply.github.com>
2022-08-07 04:01:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ WORKING_DIR=~/
 
 # Functions For Telegram Post
 msg() {
-	curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "$TG_CHAT_ID" \
+	curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+        -d "$TG_CHAT_ID" \
 	-d "disable_web_page_preview=true" \
 	-d "parse_mode=html" \
 	-d text="$1"
