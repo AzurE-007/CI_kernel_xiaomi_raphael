337840024ee6921563de977cb09bf3c4b897d29f
back-up-git <100161001+back-up-git@users.noreply.github.com>
2022-08-07 20:20:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -69,7 +69,7 @@ export KBUILD_BUILD_USER="Azure"
 export KBUILD_BUILD_HOST="Server"
 
 #Starting Compilation
-msg "<b>$BUILD_ID CI Build Triggered</b>"
+msg "<b>$BUILD_ID CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>"
 
 BUILD_START=$(date +"%s")
 export ARCH=arm64
