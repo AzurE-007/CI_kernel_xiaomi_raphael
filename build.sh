13b355ed4205844a3fc6800ac765e4f356467914
back-up-git <100161001+back-up-git@users.noreply.github.com>
2022-08-09 03:59:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ file() {
 	curl -F document=@"$1" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$TG_CHAT_ID \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-	-F caption="<b> Build took : <b><code>$2</code>%0A<b>MD5 Checksum : </b><code>$MD5</code>"
+	-F caption="<b>Build took : <b><code>$2</code>%0A<b>MD5 Checksum : </b><code>$MD5</code>"
 }
 
 # Cloning Anykernel
