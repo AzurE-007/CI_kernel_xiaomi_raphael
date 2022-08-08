8d0a369103aa4aca18da4b1d5281a350faa7a4cc
back-up-git <100161001+back-up-git@users.noreply.github.com>
2022-08-08 23:36:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ if [ -e $WORKING_DIR/Anykernel/anykernel.sh ]; then
 cd $WORKING_DIR/Anykernel
 zip -r9 $ZIP_NAME * -x .git README.md *placeholder
 cp $ZIP_NAME $WORKING_DIR/
-file "$WORKING_DIR/Anykernel/$ZIP_NAME" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+curl -F document=@"$WORKING_DIR/Anykernel/$ZIP_NAME" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" -F chat_id="$TG_CHAT_ID" -F "parse_mode=Markdown" -F caption="*✅ Build finished after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds*"
 else
 file "$WORKING_DIR/Anykernel/anykernel.sh" "Build Failed and took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 fi
