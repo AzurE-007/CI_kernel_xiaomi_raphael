36d038edca7d75b7098e627768d88c223ad29055
back-up-git <100161001+back-up-git@users.noreply.github.com>
2022-08-09 01:32:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -40,9 +40,7 @@ DIFF=$((BUILD_END - BUILD_START))
 #Zipping & Uploading Flashable Kernel Zip
 cd $WORKING_DIR/Anykernel
 zip -r9 "$ZIP_NAME.zip" * -x .git README.md *placeholder
-cp -f $WORKING_DIR/Anykernel/IMMENSiTY.zip $WORK_DIR/
-rm $WORKING_DIR/Anykernel/IMMENSiTY.zip
-file "$WORKING_DIR/IMMENSiTY.zip" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+file "$WORKING_DIR/Anykerne/IMMENSiTY.zip" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 else
 file "$WORKING_DIR/Anykernel/anykernel.sh" "Build Failed and took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 fi
