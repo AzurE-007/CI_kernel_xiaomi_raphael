6fa64774850254212432a42b5117911389913287
back-up-git <100161001+back-up-git@users.noreply.github.com>
2022-08-09 04:16:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ file() {
 	MD5=$(md5sum "$1" | cut -d' ' -f1)
 	curl -F document=@"$1" https://api.telegram.org/bot$BOT_TOKEN/sendDocument?chat_id=$TG_CHAT_ID \
         -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-	-F caption="<b> Test </b> $2 | <b>MD5 Checksum : </b><code>$MD5</code>"
+        -F "parse_mode=markdown" \
+	-F caption="<b> Test </b> $2 | *MD5 Checksum : *\`$MD5\`""
 }
 
 # Cloning Anykernel
